0dbd1b93ec4d5e962085fd986c90396895e52ce2
Randall-B24 <ranbermudez24@gmail.com>
2021-05-17 20:07:39 -0600

    Update Form1.Designer.cs

--- PROYECTO1/Form1.Designer.cs
+++ PROYECTO1/Form1.Designer.cs
@@ -29,21 +29,39 @@ namespace PROYECTO1
         /// </summary>
         private void InitializeComponent()
         {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.checkBox1.ForeColor = System.Drawing.Color.Coral;
+            this.checkBox1.Location = new System.Drawing.Point(478, 224);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(141, 21);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Inicio de proyecto";
+            this.checkBox1.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.checkBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 

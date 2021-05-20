8e7cd4c74baeacb0476197165b1da04f90a667f0
Randall-B24 <ranbermudez24@gmail.com>
2021-05-20 00:04:22 -0600

    A ver qué

--- PROYECTO1/G_Mov_Ent.cs
+++ PROYECTO1/G_Mov_Ent.cs
@@ -30,22 +30,22 @@ namespace PROYECTO1
 
         private void Nuevo_Click(object sender, EventArgs e)
         {
-            int i = Convert.ToInt32(textBox2.Text);
-            cn.InsertarMovimiento(textBox1.Text, i, textBox3.Text);
-            ConsultaMovDG.DataSource = cn.ConsultaDT();
+            //int i = Convert.ToInt32(textBox2.Text);
+            //cn.InsertarMovimiento(textBox1.Text, i, textBox3.Text);
+            //ConsultaMovDG.DataSource = cn.ConsultaDT();
         }
         private void Modificar_Click(object sender, EventArgs e)
         {
-            int i = Convert.ToInt32(textBox2.Text);
-            cn.ModificarMovimiento(textBox1.Text, i, textBox3.Text);
-            ConsultaMovDG.DataSource = cn.ConsultaDT();
+            //int i = Convert.ToInt32(textBox2.Text);
+            //cn.ModificarMovimiento(textBox1.Text, i, textBox3.Text);
+          ConsultaMovDG.DataSource = cn.ConsultaDT();
         }
 
         private void Eliminar_Click(object sender, EventArgs e)
         {
-            int i = Convert.ToInt32(textBox2.Text);
-            cn.EliminarMovimiento(textBox1.Text, i, textBox3.Text);
-            ConsultaMovDG.DataSource = cn.ConsultaDT();
+          // int i = Convert.ToInt32(textBox2.Text);
+           // cn.EliminarMovimiento(textBox1.Text, i, textBox3.Text);
+            //ConsultaMovDG.DataSource = cn.ConsultaDT();
 
         }
 

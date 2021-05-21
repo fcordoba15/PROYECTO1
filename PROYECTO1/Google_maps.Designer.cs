7d922d12c5c9afb85452e4faa57f03d186862016
fcordoba15 <80478493+fcordoba15@users.noreply.github.com>
2021-05-20 22:10:53 -0600

    GMAPS

--- PROYECTO1/Google_maps.Designer.cs
+++ PROYECTO1/Google_maps.Designer.cs
@@ -29,21 +29,18 @@ namespace PROYECTO1
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.txtUbicacion = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLatitud = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLongitud = new System.Windows.Forms.TextBox();
+            this.guardar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
-            this.label1.Location = new System.Drawing.Point(124, 106);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(221, 37);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Aquí va Gmap";
             // 
             // gMapControl1
             // 
@@ -53,7 +50,7 @@ namespace PROYECTO1
             this.gMapControl1.GrayScaleMode = false;
             this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
             this.gMapControl1.LevelsKeepInMemmory = 5;
-            this.gMapControl1.Location = new System.Drawing.Point(47, 33);
+            this.gMapControl1.Location = new System.Drawing.Point(12, 9);
             this.gMapControl1.MarkersEnabled = true;
             this.gMapControl1.MaxZoom = 2;
             this.gMapControl1.MinZoom = 2;
@@ -67,29 +64,111 @@ namespace PROYECTO1
             this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
             this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
             this.gMapControl1.ShowTileGridLines = false;
-            this.gMapControl1.Size = new System.Drawing.Size(150, 150);
+            this.gMapControl1.Size = new System.Drawing.Size(232, 308);
             this.gMapControl1.TabIndex = 1;
             this.gMapControl1.Zoom = 0D;
+            this.gMapControl1.Load += new System.EventHandler(this.gMapControl1_Load);
+            // 
+            // txtUbicacion
+            // 
+            this.txtUbicacion.Location = new System.Drawing.Point(305, 27);
+            this.txtUbicacion.Name = "txtUbicacion";
+            this.txtUbicacion.Size = new System.Drawing.Size(126, 20);
+            this.txtUbicacion.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(302, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ubicación";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(302, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Latitud";
+            // 
+            // txtLatitud
+            // 
+            this.txtLatitud.Location = new System.Drawing.Point(305, 75);
+            this.txtLatitud.Name = "txtLatitud";
+            this.txtLatitud.Size = new System.Drawing.Size(126, 20);
+            this.txtLatitud.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(302, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Longitud";
+            // 
+            // txtLongitud
+            // 
+            this.txtLongitud.Location = new System.Drawing.Point(305, 123);
+            this.txtLongitud.Name = "txtLongitud";
+            this.txtLongitud.Size = new System.Drawing.Size(126, 20);
+            this.txtLongitud.TabIndex = 7;
+            // 
+            // guardar
+            // 
+            this.guardar.Location = new System.Drawing.Point(324, 149);
+            this.guardar.Name = "guardar";
+            this.guardar.Size = new System.Drawing.Size(75, 23);
+            this.guardar.TabIndex = 8;
+            this.guardar.Text = "Guardar";
+            this.guardar.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(305, 185);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(162, 132);
+            this.dataGridView1.TabIndex = 9;
             // 
             // Google_maps
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(474, 273);
-            this.Controls.Add(this.gMapControl1);
+            this.ClientSize = new System.Drawing.Size(489, 329);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.guardar);
+            this.Controls.Add(this.txtLongitud);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtLatitud);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Controls.Add(this.txtUbicacion);
+            this.Controls.Add(this.gMapControl1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Google_maps";
             this.Text = "Google_maps";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private GMap.NET.WindowsForms.GMapControl gMapControl1;
+        private System.Windows.Forms.TextBox txtUbicacion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLatitud;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLongitud;
+        private System.Windows.Forms.Button guardar;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- PROYECTO1/Google_maps.cs
+++ PROYECTO1/Google_maps.cs
@@ -8,13 +8,57 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+
 namespace PROYECTO1
 {
     public partial class Google_maps : Form
     {
+        GMarkerGoogle marker;
+        GMapOverlay markerOverlay;
+        DataTable dt;
+
+        int filaSeleccionada = 0;
+        double LatInicial = 20.96881328132813906;
+        double LngInicial = -89.6250915527344;
+
         public Google_maps()
         {
             InitializeComponent();
+        }
+
+        
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gMapControl1_Load(object sender, EventArgs e)
+        {
+            gMapControl1.DragButton = MouseButtons.Left;
+            gMapControl1.CanDragMap = true;
+            gMapControl1.MapProvider = GMapProviders.GoogleMap;
+            gMapControl1.Position = new PointLatLng(LatInicial, LngInicial);
+            gMapControl1.MinZoom = 0;
+            gMapControl1.MaxZoom = 24;
+            gMapControl1.Zoom = 9;
+            gMapControl1.AutoScroll = true;
+
+            //MARCADOR
+            markerOverlay = new GMapOverlay("Marcador");
+            marker = new GMarkerGoogle(new PointLatLng(LatInicial, LngInicial), GMarkerGoogleType.red);
+            markerOverlay.Markers.Add(marker); //agregar al mapa
+
+            //agregar un tooltip de texto a marcadores
+            marker.ToolTipMode = MarkerTooltipMode.Always;
+            marker.ToolTipText = string.Format("Ubicación: \n Latitud: {0} Longitud: {1}", LatInicial, LngInicial);
+
+            gMapControl1.Overlays.Add(markerOverlay);
+            
         }
     }
 }

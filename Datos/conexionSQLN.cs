c3467d3547bcc9785864d648e8f75b34c52c09e6
Randall-B24 <ranbermudez24@gmail.com>
2021-05-27 11:46:26 -0600

    Cambios locos

--- Datos/conexionSQLN.cs
+++ Datos/conexionSQLN.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Datos;
 using System.Data;
+using System.Windows.Forms;
 
 
 namespace Negocios
--- Negocios/ConexionSQL.cs
+++ Negocios/ConexionSQL.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Data;
 using System.Drawing;
 using System.Data.SqlClient;
+using System.Windows.Forms;
 
 namespace Datos
 {

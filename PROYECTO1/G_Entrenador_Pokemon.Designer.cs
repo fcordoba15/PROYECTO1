174f15dde5d4b309bd3aea0f7c764ea98604e452
fcordoba15 <80478493+fcordoba15@users.noreply.github.com>
2021-05-28 01:24:08 -0600

    entrenador

--- PROYECTO1/G_Entrenador_Pokemon.Designer.cs
+++ PROYECTO1/G_Entrenador_Pokemon.Designer.cs
@@ -40,13 +40,16 @@ namespace PROYECTO1
             this.txt_id_entrenador_pokemon = new System.Windows.Forms.TextBox();
             this.txt_id_pokemon_entrenador = new System.Windows.Forms.TextBox();
             this.Estado = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.PokemonesEntrenadorDG)).BeginInit();
             this.SuspendLayout();
             // 
             // PokemonesEntrenadorDG
             // 
             this.PokemonesEntrenadorDG.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.PokemonesEntrenadorDG.Location = new System.Drawing.Point(0, 2);
+            this.PokemonesEntrenadorDG.Location = new System.Drawing.Point(6, 81);
             this.PokemonesEntrenadorDG.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.PokemonesEntrenadorDG.Name = "PokemonesEntrenadorDG";
             this.PokemonesEntrenadorDG.RowHeadersWidth = 82;
@@ -56,18 +59,19 @@ namespace PROYECTO1
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(20, 309);
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(8, 386);
             this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(58, 22);
             this.button1.TabIndex = 1;
             this.button1.Text = "Agregar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(114, 309);
+            this.button2.Location = new System.Drawing.Point(70, 386);
             this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(58, 22);
@@ -78,18 +82,19 @@ namespace PROYECTO1
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(215, 309);
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Location = new System.Drawing.Point(132, 386);
             this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(60, 22);
             this.button3.TabIndex = 3;
             this.button3.Text = "Eliminar";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(508, 309);
+            this.button4.Location = new System.Drawing.Point(525, 386);
             this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(76, 22);
@@ -100,7 +105,7 @@ namespace PROYECTO1
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(20, 208);
+            this.label1.Location = new System.Drawing.Point(18, 314);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(73, 13);
@@ -110,7 +115,7 @@ namespace PROYECTO1
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(114, 207);
+            this.label2.Location = new System.Drawing.Point(96, 314);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(66, 13);
@@ -120,7 +125,7 @@ namespace PROYECTO1
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(215, 207);
+            this.label3.Location = new System.Drawing.Point(186, 314);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(40, 13);
@@ -129,7 +134,7 @@ namespace PROYECTO1
             // 
             // txt_id_entrenador_pokemon
             // 
-            this.txt_id_entrenador_pokemon.Location = new System.Drawing.Point(22, 253);
+            this.txt_id_entrenador_pokemon.Location = new System.Drawing.Point(21, 341);
             this.txt_id_entrenador_pokemon.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_id_entrenador_pokemon.Name = "txt_id_entrenador_pokemon";
             this.txt_id_entrenador_pokemon.Size = new System.Drawing.Size(52, 20);
@@ -137,7 +142,7 @@ namespace PROYECTO1
             // 
             // txt_id_pokemon_entrenador
             // 
-            this.txt_id_pokemon_entrenador.Location = new System.Drawing.Point(117, 252);
+            this.txt_id_pokemon_entrenador.Location = new System.Drawing.Point(99, 341);
             this.txt_id_pokemon_entrenador.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_id_pokemon_entrenador.Name = "txt_id_pokemon_entrenador";
             this.txt_id_pokemon_entrenador.Size = new System.Drawing.Size(52, 20);
@@ -150,17 +155,53 @@ namespace PROYECTO1
             "En equipo",
             "En reserva",
             "Liberado"});
-            this.Estado.Location = new System.Drawing.Point(215, 251);
+            this.Estado.Location = new System.Drawing.Point(179, 341);
             this.Estado.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Estado.Name = "Estado";
             this.Estado.Size = new System.Drawing.Size(80, 21);
             this.Estado.TabIndex = 11;
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(7, 283);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(504, 17);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "______________________________________________________________";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(7, 266);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(96, 17);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Información";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(476, 42);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "POKEMON - ENTRENADOR";
+            // 
             // GestionPokemonesEntrenador
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(596, 351);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(612, 419);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.Estado);
             this.Controls.Add(this.txt_id_pokemon_entrenador);
             this.Controls.Add(this.txt_id_entrenador_pokemon);
@@ -195,5 +236,8 @@ namespace PROYECTO1
         private System.Windows.Forms.TextBox txt_id_entrenador_pokemon;
         private System.Windows.Forms.TextBox txt_id_pokemon_entrenador;
         private System.Windows.Forms.ComboBox Estado;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
     }
 }

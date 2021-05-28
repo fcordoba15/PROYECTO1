6dbe478927183b5eb9aa41ffb4a9c24de2a85847
fcordoba15 <80478493+fcordoba15@users.noreply.github.com>
2021-05-28 16:16:25 -0600

    2 validacion

--- PROYECTO1/G_Entrenador_Pokemon.cs
+++ PROYECTO1/G_Entrenador_Pokemon.cs
@@ -33,24 +33,41 @@ namespace PROYECTO1
 
         private void button2_Click(object sender, EventArgs e)
         {
-            int i2 = Convert.ToInt32(txt_id_pokemon_entrenador.Text);
-            cn.ModificarPokemonEntrenador(txt_id_entrenador_pokemon.Text, i2, Estado.GetItemText(Estado.SelectedItem));
-            PokemonesEntrenadorDG.DataSource = cn.ConsultaPokemonesEntrenador();
-            MessageBox.Show("¡Modificado!");
-            ModuloEntrenador v1 = new ModuloEntrenador();
-            this.Hide();
-            v1.Show();
+            try
+            {
+                int i2 = Convert.ToInt32(txt_id_pokemon_entrenador.Text);
+                cn.ModificarPokemonEntrenador(txt_id_entrenador_pokemon.Text, i2, Estado.GetItemText(Estado.SelectedItem));
+                PokemonesEntrenadorDG.DataSource = cn.ConsultaPokemonesEntrenador();
+                MessageBox.Show("¡Modificado!");
+                ModuloEntrenador v1 = new ModuloEntrenador();
+                this.Hide();
+                v1.Show();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Error" + ex);
+            }
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            int i2 = Convert.ToInt32(txt_id_pokemon_entrenador.Text);
-            cn.EliminarPokemonEntrenador(txt_id_entrenador_pokemon.Text, i2);
-            PokemonesEntrenadorDG.DataSource = cn.ConsultaPokemonesEntrenador();
-            MessageBox.Show("¡Eliminado con éxito!");
-            ModuloEntrenador v1 = new ModuloEntrenador();
-            this.Hide();
-            v1.Show();
+
+            try
+            {
+                int i2 = Convert.ToInt32(txt_id_pokemon_entrenador.Text);
+                cn.EliminarPokemonEntrenador(txt_id_entrenador_pokemon.Text, i2);
+                PokemonesEntrenadorDG.DataSource = cn.ConsultaPokemonesEntrenador();
+                MessageBox.Show("¡Eliminado con éxito!");
+                ModuloEntrenador v1 = new ModuloEntrenador();
+                this.Hide();
+                v1.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:"+ex);
+
+            }
 
         }
 

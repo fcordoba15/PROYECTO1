8482dd0820da8e4791d11730cbafddab2e74000a
fabyhm17 <fabihm17@gmail.com>
2021-05-28 14:13:34 -0600

    validaciones mov-entrenador

--- PROYECTO1/G_Entrenador_Movimiento.cs
+++ PROYECTO1/G_Entrenador_Movimiento.cs
@@ -28,16 +28,85 @@ namespace PROYECTO1
 
         private void Nuevo_Click(object sender, EventArgs e)
         {
-            int i = Convert.ToInt32(textBox2.Text);
-            cn.InsertarMovimiento(textBox1.Text, i, textBox3.Text);
-            ConsultaMovDG.DataSource = cn.ConsultaDT();
+            //--------------------------------- VALIDACIONES DE ESCRITURA EN CAMPOS ---------------------------------
+            int a = 0;
+            while (a == 0)
+            {
+                //------------- VALIDACIONES DE COMILLAS -------------
+                string p = "'";
+                int validación_comilla = 0;
+                foreach (char c in textBox3.Text)
+                {
+                    if (c == p[0])
+                        validación_comilla++;
+                }
+                if (validación_comilla > 0)
+                {
+                    MessageBox.Show("¡Error! El ID del movimiento no debe tener comillas simples");
+                    break;
+                }
+
+                //------------- VALIDACIONES DE DATOS NUMERICOS -------------
+                if (textBox1.Text.All(char.IsDigit) == false)
+                {
+                    MessageBox.Show("¡Error! El ID del entrenador debe ser numérico");
+                    break;
+                }
+                if (textBox2.Text.All(char.IsDigit) == false)
+                {
+                    MessageBox.Show("¡Error! El ID del Pokemon debe ser numérico");
+                    break;
+                }
+
+                //------------- REGISTRO EN LA BASE DE DATOS -------------
+                int i = Convert.ToInt32(textBox2.Text);
+                cn.InsertarMovimiento(textBox1.Text, i, textBox3.Text);
+                ConsultaMovDG.DataSource = cn.ConsultaDT();
+                a++;
+            }
         }
+
+
         private void Modificar_Click(object sender, EventArgs e)
         {
-            int i = Convert.ToInt32(textBox2.Text);
-            cn.ModificarMovimiento(textBox1.Text, i, textBox3.Text);
-            ConsultaMovDG.DataSource = cn.ConsultaDT();
+            //--------------------------------- VALIDACIONES DE ESCRITURA EN CAMPOS ---------------------------------
+            int a = 0;
+            while (a == 0)
+            {
+                //------------- VALIDACIONES DE COMILLAS -------------
+                string p = "'";
+                int validación_comilla = 0;
+                foreach (char c in textBox3.Text)
+                {
+                    if (c == p[0])
+                        validación_comilla++;
+                }
+                if (validación_comilla > 0)
+                {
+                    MessageBox.Show("¡Error! El ID del movimiento no debe tener comillas simples");
+                    break;
+                }
+
+                //------------- VALIDACIONES DE DATOS NUMERICOS -------------
+                if (textBox1.Text.All(char.IsDigit) == false)
+                {
+                    MessageBox.Show("¡Error! El ID del entrenador debe ser numérico");
+                    break;
+                }
+                if (textBox2.Text.All(char.IsDigit) == false)
+                {
+                    MessageBox.Show("¡Error! El ID del Pokemon debe ser numérico");
+                    break;
+                }
+
+                //------------- REGISTRO EN LA BASE DE DATOS -------------
+                int i = Convert.ToInt32(textBox2.Text);
+                cn.ModificarMovimiento(textBox1.Text, i, textBox3.Text);
+                ConsultaMovDG.DataSource = cn.ConsultaDT();
+                a++;
+            }
         }
+
 
         private void Eliminar_Click(object sender, EventArgs e)
         {
